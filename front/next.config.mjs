65e8708d62c0e102135241a5ba3aa4ec7faf2063
Daniel Alexander Muelas Rivera <danielmuelasrivera483@gmail.com>
2024-08-14 22:32:06 -0500

    changing front

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'via.placeholder.com', // Agrega el dominio del que traerás las imágenes
-            pathname: '/**',
-          },
-        ],
-      },
-};
+const nextConfig = {};
 
 export default nextConfig;
