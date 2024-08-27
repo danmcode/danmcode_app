66cd4081f670589fd098c5193a1e9b5a7730788b
Daniel Muelas <danmcode@gmail.com>
2024-08-27 17:00:12 -0500

    front changes

--- front/src/app/admin/configuration/page.tsx
+++ front/src/app/admin/configuration/page.tsx
@@ -1,54 +1,75 @@
-import Breadcrumbs from "@/app/ui/components/breadcrumb";
-import ConfigMenu from "@/app/ui/components/config/menu";
-import ConfigMenuContent from "@/app/ui/components/config/menu-content";
-import { ConfigMenuItems } from "@/app/ui/components/config/menu-items";
+"use client";
+
+
+import { useState, useEffect } from 'react';
+import Breadcrumbs from '@/app/ui/components/breadcrumb';
+import ConfigMenu from '@/app/ui/components/config/menu';
+import ConfigMenuContent from '@/app/ui/components/config/menu-content';
+import { ConfigMenuItems } from '@/app/ui/components/config/menu-items';
 
 export default function ConfigurationPage() {
+    // Inicializa el estado con el ID del primer ítem del menú
+    const [selectedMenuItemId, setSelectedMenuItemId] = useState<string | null>(null);
 
     const breadcrumbs = [
         { label: 'Inicio', href: '/admin' },
         { label: 'Configuración', href: '/admin/configuration', active: true },
     ];
+
+    useEffect(() => {
+        // Establece el primer ítem como seleccionado al cargar el componente
+        if (ConfigMenuItems.length > 0) {
+            setSelectedMenuItemId(ConfigMenuItems[0].id);
+        }
+    }, []);
+
+    const handleMenuItemClick = (id: string) => {
+        setSelectedMenuItemId(id);
+    };
+
+    // Encuentra el contenido del menú seleccionado
+    const selectedMenuItem = ConfigMenuItems.find(item => item.id === selectedMenuItemId);
+
     return (
         <div>
             <Breadcrumbs breadcrumbs={breadcrumbs} pageTitle="Configuración" />
 
             <div className="row g-0 border-top border-bottom bg-color-primary">
-
                 <div className="col-sm-4 h-100">
                     <div
                         className="nav flex-sm-column border-bottom border-bottom-sm-0 border-end-sm fs-9 vertical-tab h-100 justify-content-between"
                         role="tablist"
-                        aria-orientation="vertical">
-
-                        {ConfigMenuItems.map((menuItem, index) => {
-                            return (
-                                <ConfigMenu configMenu={{
+                        aria-orientation="vertical"
+                    >
+                        {ConfigMenuItems.map((menuItem, index) => (
+                            <ConfigMenu
+                                key={menuItem.id}
+                                configMenu={{
                                     icon: menuItem.icon,
                                     id: menuItem.id,
                                     target: menuItem.target,
                                     name: menuItem.name,
-                                    index: index
-                                }}></ConfigMenu>
-                            );
-                        })}
-
+                                    index: index,
+                                }}
+                                onclick={() => handleMenuItemClick(menuItem.id)} />
+                        ))}
                     </div>
                 </div>
 
                 <div className="col-sm-8">
                     <div className="tab-content py-3 ps-sm-4 h-100">
                         {ConfigMenuItems.map((menuItem, index) => {
-                            return (
-                                <ConfigMenuContent
-                                    configMenu={{
-                                        id: menuItem.target,
-                                        index: index,
-                                        children: menuItem.content
-                                    }}
-                                />
-                            );
-                        })}
+
+                                return (
+                                    <ConfigMenuContent
+                                        configMenu={{
+                                            id: menuItem.target,
+                                            index: index,
+                                            children: menuItem.content
+                                        }}
+                                    />
+                                );
+                            })}
                     </div>
                 </div>
             </div>
--- front/src/app/ui/components/config/menu.tsx
+++ front/src/app/ui/components/config/menu.tsx
@@ -8,11 +8,12 @@ interface Props {
         target: string;
         name: String;
         index: number
-    }
+    };
+    onclick: (id: string) => void;
 }
 
 export default function ConfigMenu(props: Props) {
-    const { configMenu } = props;
+    const { configMenu, onclick} = props;
 
     return (
         <a className={`nav-link border-end border-end-sm-0 border-bottom-sm text-center text-sm-start cursor-pointer outline-none d-sm-flex align-items-sm-center ${configMenu.index === 0 ? 'active' : ''}`}
@@ -21,7 +22,9 @@ export default function ConfigMenu(props: Props) {
             data-bs-target={`#${configMenu.target}`}
             role="tab"
             aria-controls={`${configMenu.target}`}
-            aria-selected="true">
+            aria-selected="true"
+            onClick={ () => onclick(configMenu.id) }
+        >
             <span className="me-sm-2 nav-iconss">
                 <FontAwesomeIcon icon={configMenu.icon} />
             </span>
