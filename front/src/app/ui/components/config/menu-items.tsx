a95acc1c54242374e5e8ed15e69da61caa4035ba
Daniel Alexander Muelas Rivera <danmcode@gmail.com>
2024-09-05 11:56:54 -0500

    working on: menu changes

--- front/src/app/ui/components/config/menu-items.tsx
+++ front/src/app/ui/components/config/menu-items.tsx
@@ -1,14 +1,14 @@
-import { faCheck, faListDots, faUsers, faUserShield } from "@fortawesome/free-solid-svg-icons";
+import { faBuilding, faCheck, faListDots, faUsers, faUserShield } from "@fortawesome/free-solid-svg-icons";
 import DropDownContent from "./content/dropdown-content";
 import RolesContent from "./content/roles/roles-content";
 
 export const ConfigMenuItems = [
     {
-        icon: faUsers,
-        id: "UsersTab",
-        target: "userTabContent",
-        name: "Usuarios",
-        content: <>Usuarios</>
+        icon: faListDots,
+        id: "dropDownListTab",
+        target: "dropDownListTabContent",
+        name: "Listas desplegables",
+        content: <DropDownContent />
     },
     {
         icon: faUserShield,
@@ -18,17 +18,24 @@ export const ConfigMenuItems = [
         content: <RolesContent />
     },
     {
+        icon: faBuilding,
+        id: "ClientsTab",
+        target: "ClientsTabContent",
+        name: "Clientes",
+        content: <>Clientes</>
+    },
+    {
+        icon: faUsers,
+        id: "UsersTab",
+        target: "userTabContent",
+        name: "Usuarios",
+        content: <>Usuarios</>
+    },
+    {
         icon: faCheck,
         id: "PermisosTab",
         target: "PermisosTabContent",
         name: "Permisos",
         content: <> Permisos </>
-    },
-    {
-        icon: faListDots,
-        id: "dropDownListTab",
-        target: "dropDownListTabContent",
-        name: "Listas desplegables",
-        content: <DropDownContent />
     },
 ]
